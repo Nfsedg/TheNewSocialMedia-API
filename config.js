52071e651ce84d0fddc505fcf7d12230011143e9
Edgar Pech <edgaropech@hotmail.com>
2022-06-01 23:34:03 -0500

    [ADD] upload image endpoint

---
+++ config.js
@@ -0,0 +1,5 @@
+const apiConfig = {
+	rootPath: __dirname
+};
+
+module.exports = apiConfig;
---
+++ controllers/upload.js
@@ -0,0 +1,43 @@
+const uploadRouter = require("express").Router();
+const fs = require("fs");
+const path = require("path");
+const multer = require("multer");
+const upload = multer({ dest: "public/images" });
+const ImageFile = require("../models/ImageFile");
+const {rootPath} = require("../config");
+
+uploadRouter.get("/", async (req, res) => {
+	const image = await ImageFile.find({});
+
+	res.json(image).end();
+
+	fs.readFile(image[0].img.data, (err, content) => {
+		if(err) {
+			res.writeHead(404, { "Content-type": "text/html" });
+			res.end("Image don't find");
+		} else {
+			res.writeHead(200, { "Content-type": "image/*" });
+			res.end(content);
+		}
+	});
+});
+
+uploadRouter.post("/", upload.single("image"), async (req, res) => {
+	// fs.renameSync(req.file.path, req.file.path + "." + req.file.mimetype.split("/")[1]);
+	let payload = new ImageFile ({
+		name: "name",
+		desc: "desc",
+		img: {
+			data: fs.readFileSync(path.join(rootPath + "/public/images/" + req.file.filename)),
+			contentType: "image/*",
+		}
+	});
+	try {
+		await payload.save();
+		res.json("Checked image");
+	} catch (e) {
+		console.log(e);
+	}
+});
+
+module.exports = uploadRouter;
